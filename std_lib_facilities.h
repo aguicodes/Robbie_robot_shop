762884ff6b77fb6ca6157fd8739034bfcb760896
Gracie <g.aguilar92@yahoo.com>
2016-10-05 18:20:18 -0500

    Create std_lib_facilities.h

---
+++ std_lib_facilities.h
@@ -0,0 +1,16 @@
+#ifndef main_h
+#define main_h
+
+#include<iostream>
+
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<fstream>
+#include<cstdlib>
+
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
+#endif 
